f0dc383cd11406930efbcc5fe37bcee16906002a
DerputeR <dekxy-developer@yahoo.com>
2025-06-24 11:06:35 +0000

    init: copied and stripped parts from elevatorsim

---
+++ README.md
@@ -0,0 +1,73 @@
+# Dependencies
+
+- SDL release-3.2.16 (fetched)
+- Dear ImGui 1.91.9b-docking (fetched)
+- glad (included)
+- GLM 1.0.1 (fetched)
+
+# Supported systems
+
+This project has been verified to build on:
+
+- Windows 11 (x64)
+- Ubuntu 24.04 LTS (amd64, arm64)
+
+This project _should_ work on a variety of other systems, but this has not been tested yet.
+
+# Installation
+
+This project uses CMake to build and install.
+
+## Windows
+
+I recommend using Ninja as the build system and MSVC 2022 x64 as the compiler, but it should work fine with other build systems and compilers.
+
+### To compile using MSVC and Ninja
+
+1. Download and install CMake 3.12 or above.
+2. Download and install [Ninja](https://github.com/ninja-build/ninja/wiki/Pre-built-Ninja-packages)
+3. Download and install [Microsoft Visual Studio Community Edition](https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&channel=Release&version=VS2022&source=VSLandingPage&cid=2030&passive=false)
+    - Ensure that the "Desktop development with C++" workload is selected with your installation.
+4. Open a Visual Studio Developer terminal instance in the root directory of the project (here, using the VS Developer Powershell profile added to Windows Terminal) and run the following:
+
+    ```powershell
+    mkdir build &&
+    cd build &&
+    cmake -G Ninja .. &&
+    cmake --build .
+    ```
+
+From here, you can run `Charcoal.exe`.
+
+### To generate a Visual Studio 17 2022 solution
+
+Modern versions of Visual Studio actually enable direct integration with CMake projects. If you follow steps 1 and 3 from the previous section, you can open the project root directory as a folder in Visual Studio, and the IDE will recognize the project as a CMake project and let you get started right away.
+
+However, if you prefer to work with the older solution-based approach:
+
+1. Follow steps 1 and 3 from the previous section
+2. Open a terminal in the root directory of the project and run the following:
+
+   ```powershell
+   mkdir build &&
+   cd build &&
+   cmake -G "Visual Studio 17 2022" ..
+   ```
+3. Open `CharcoalProject.sln` in Visual Studio Community Edition.
+
+## Linux
+
+This process is similar to the Windows build process.
+
+1. Download and setup CMake 3.21 or above (see [here](https://www.linuxfordevices.com/tutorials/linux/install-cmake-on-linux) for a guide)
+2. (Optional) Download and setup [Ninja](https://github.com/ninja-build/ninja/wiki/Pre-built-Ninja-packages).
+3. Open a terminal at the root directory of the project and run the following:
+
+    ```bash
+    mkdir build &&
+    cd build &&
+    cmake -G Ninja .. &&
+    cmake --build .
+    ```
+
+When all of the above are complete, you can run `Charcoal`.
---
+++ src/main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <array>
+
+#include <glad/glad.h>
+#include <SDL3/SDL.h>
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+#include <imgui.h>
+#include <backends/imgui_impl_sdl3.h>
+#include <backends/imgui_impl_sdlrenderer3.h>
+
+#include "engine/shader_loader.h"
+
+#include "app_info.h"
+
+static SDL_Window* window;
+static SDL_Renderer* renderer; // for ease of drawing stuff until I can convert to pure opengl
+
+static Scene scene{};
+
+static const Uint64 ONE_SECOND_NS = 1000000000;
+
+static bool vsync_enabled = false;
+static bool vsync_adaptive = true;
+static Uint64 min_frame_time = ONE_SECOND_NS / 120;
+static Uint64 time_ns_last = 0;
+static Uint64 time_ns_now = 0;
+static Uint64 time_ns_delta = 0;
+
+static Uint64 frame_number = 0;
+
+static void update_frame_cap(Uint64 frame_cap) {
+    if (frame_cap == 0) {
+        min_frame_time = 0;
+    }
+    else {
+        min_frame_time = ONE_SECOND_NS / frame_cap;
+    }
+}
+
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
+    SDL_SetAppMetadata(APP_FULL_NAME, APP_VERSION, APP_PACKAGE);
+    SDL_SetLogPriority(SDL_LOG_CATEGORY_VIDEO, SDL_LOG_PRIORITY_DEBUG);
+    SDL_SetLogPriority(SDL_LOG_CATEGORY_RENDER, SDL_LOG_PRIORITY_DEBUG);
+    SDL_SetLogPriority(SDL_LOG_CATEGORY_CUSTOM, SDL_LOG_PRIORITY_DEBUG);
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_ASSERT, "SDL failed to init: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // Window init
+    window = SDL_CreateWindow(APP_WINDOW_TITLE, 800, 600, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    if (window == nullptr) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Failed to create window: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // SDL renderer init
+    renderer = SDL_CreateRenderer(window, NULL);
+    if (renderer == nullptr) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_RENDER, "Failed to create SDL renderer for window: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    
+    if (vsync_enabled) {
+        if (vsync_adaptive) {
+            if (!SDL_SetRenderVSync(renderer, -1)) {
+                SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to enable adaptive vsync: %s", SDL_GetError());
+                if (!SDL_SetRenderVSync(renderer, 1)) {
+                    SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to enable vsync: %s", SDL_GetError());
+                }
+            }
+        } else if (!SDL_SetRenderVSync(renderer, 1)) {
+            SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to enable vsync: %s", SDL_GetError());
+        }
+    } else if (!SDL_SetRenderVSync(renderer, 0)) {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to disable vsync: %s", SDL_GetError());
+    }
+    
+    // Dear ImGUI init
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+    io.IniFilename = NULL;
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate) {
+    // compute previous frame time
+    time_ns_last = time_ns_now;
+    time_ns_now = SDL_GetTicksNS();
+    time_ns_delta = time_ns_now - time_ns_last;
+
+    // manual framecap when vsync is off
+    if ((!vsync_enabled || (vsync_enabled && vsync_adaptive)) && min_frame_time > 0) {
+        
+        if (time_ns_delta < min_frame_time) {
+            SDL_DelayPrecise(min_frame_time - time_ns_delta);
+            time_ns_now = SDL_GetTicksNS();
+            time_ns_delta = time_ns_now - time_ns_last;
+        }
+    }
+
+    // update the scene
+    scene.update(time_ns_delta / static_cast<float>(ONE_SECOND_NS));
+
+    // clear the buffer    
+    SDL_SetRenderDrawColor(renderer, 32, 32, 32, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
+    // draw the scene
+
+    // draw the GUI
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+    ImGui::Render();
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+
+    // display the render
+    SDL_RenderPresent(renderer);
+
+    frame_number++;
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event *event) {
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS;
+    }
+    ImGui_ImplSDL3_ProcessEvent(event);
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+    SDL_DestroyWindow(window);
+}
