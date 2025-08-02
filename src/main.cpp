ea5d136a4f113a2fc5863c3116b8d3c925f28ab5
DerputeR <dekxy-developer@yahoo.com>
2025-08-02 16:30:23 -0400

    feat: re-added opengl3 backend

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
+#include "SDL3/SDL_error.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_log.h"
+#include "SDL3/SDL_pixels.h"
 #include "SDL3/SDL_timer.h"
-#include <iostream>
-#include <array>
+#include "SDL3/SDL_video.h"
 
 #include <glad/glad.h>
 #include <SDL3/SDL.h>
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_opengl.h>
 
 #include <imgui.h>
-#include <backends/imgui_impl_sdl3.h>
-#include <backends/imgui_impl_sdlrenderer3.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_opengl3.h>
 
 #include "engine/time.h"
 #include "engine/shader_loader.h"
@@ -19,9 +23,10 @@
 using namespace Charcoal;
 
 static SDL_Window* window;
-static SDL_Renderer* renderer; // for ease of drawing stuff until I can convert to pure opengl
+static SDL_GLContext glCtx;
 
 static Engine::Time engine_time;
+static int fps_max = 400; // TODO: move to config
 
 static bool vsync_enabled = false;
 static bool vsync_adaptive = true;
@@ -38,48 +43,70 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv) {
     }
 
     // Window init
-    window = SDL_CreateWindow(APP_WINDOW_TITLE, 1280, 720, SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY);
+    window = SDL_CreateWindow(APP_WINDOW_TITLE, 1280, 720,
+                              SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_OPENGL);
     if (window == nullptr) {
         SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Failed to create window: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    // SDL renderer init
-    renderer = SDL_CreateRenderer(window, NULL);
-    if (renderer == nullptr) {
-        SDL_LogCritical(SDL_LOG_CATEGORY_RENDER, "Failed to create SDL renderer for window: %s", SDL_GetError());
+    // OpenGL render context
+    glCtx = SDL_GL_CreateContext(window);
+    if (glCtx == nullptr) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Failed to create OpenGL context for window: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    
+    if (!SDL_GL_MakeCurrent(window, glCtx)) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Failed to set context as current for window: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
     if (vsync_enabled) {
         if (vsync_adaptive) {
-            if (!SDL_SetRenderVSync(renderer, -1)) {
-                SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to enable adaptive vsync: %s", SDL_GetError());
-                if (!SDL_SetRenderVSync(renderer, 1)) {
+            if (!SDL_GL_SetSwapInterval(-1)) {
+                SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to enable adaptive vsync, retrying with normal vsync: %s", SDL_GetError());
+                if (!SDL_GL_SetSwapInterval(1)) {
                     SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to enable vsync: %s", SDL_GetError());
                 }
             }
-        } else if (!SDL_SetRenderVSync(renderer, 1)) {
+        } else if (!SDL_GL_SetSwapInterval(1)) {
             SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to enable vsync: %s", SDL_GetError());
         }
-    } else if (!SDL_SetRenderVSync(renderer, 0)) {
+    } else if (!SDL_GL_SetSwapInterval(0)) {
         SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to disable vsync: %s", SDL_GetError());
     } else {
-        engine_time.set_fps_cap(360);
+        engine_time.set_fps_cap(fps_max);
+    }
+
+
+    // GLAD init
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Failed to initialize GLAD");
+        return SDL_APP_FAILURE;
+    }
+
+    // Test shader loader
+    GLuint default_program = ShaderLoader::create_program(ShaderLoader::DEFAULT_VERT_SRC, ShaderLoader::DEFAULT_FRAG_SRC);
+    if (default_program == 0) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Failed to create default shader program");
+        return SDL_APP_FAILURE;
     }
 
     // Dear ImGUI init
     IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
+    if (ImGui::CreateContext() == nullptr) {
+        SDL_LogCritical(SDL_LOG_CATEGORY_VIDEO, "Failed to create ImGui Context");
+        return SDL_APP_FAILURE;
+    }
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
-    io.IniFilename = NULL;
+    io.IniFilename = nullptr; // do not load from ini file. we can customize this later
 
     // Setup Platform/Renderer backends
-    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
-    ImGui_ImplSDLRenderer3_Init(renderer);
+    ImGui_ImplSDL3_InitForOpenGL(window, glCtx);
+    ImGui_ImplOpenGL3_Init("#version 330 core"); // glad was configured to use 3.3 core
 
     return SDL_APP_CONTINUE;
 }
@@ -103,22 +130,23 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
     // update the scene
     // TODO
 
-    // clear the buffer    
-    SDL_SetRenderDrawColor(renderer, 32, 32, 32, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
+    // clear the buffer
+    glClearColor(32.0f/255.0f, 32.0f/255.0f, 32.0f/255.0f, SDL_ALPHA_OPAQUE_FLOAT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // draw the scene
+    // TODO
 
     // draw the GUI
-    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplSDL3_NewFrame();
     ImGui::NewFrame();
     ImGui::ShowDemoWindow(&demo_window_shown);
     ImGui::Render();
-    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
     // display the render
-    SDL_RenderPresent(renderer);
+    SDL_GL_SwapWindow(window);
 
     return SDL_APP_CONTINUE;
 }
@@ -132,7 +160,7 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event *event) {
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplSDL3_Shutdown();
     ImGui::DestroyContext();
     SDL_DestroyWindow(window);
