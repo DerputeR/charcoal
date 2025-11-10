09d5975723d10e4e5ea2696797ed44066748f48b
DerputeR <dekxy-developer@yahoo.com>
2025-11-10 15:25:12 -0500

    feat: back to a triangle

--- src/scenes/triangle_scene.h
+++ src/scenes/triangle_scene.h
@@ -4,8 +4,8 @@
 namespace Charcoal {
 class TriangleScene : public Scene {
     std::vector<Vertex> verts{{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}}, {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
-            {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}}, {{-0.5f, 0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}}};
-    std::vector<int> indices{2, 0, 1, 2, 3, 0};
+            {{0.0f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}}};
+    std::vector<int> indices{2, 0, 1};
     std::vector<Mesh> meshes = {{verts, indices}};
 
 public:
