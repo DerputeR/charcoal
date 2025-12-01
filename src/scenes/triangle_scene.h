f413ad7c2a2954f31c2a38c2d350dd8cbccb0311
DerputeR <dekxy-developer@yahoo.com>
2025-12-01 00:12:45 -0500

    feat: specified uv coords

--- src/scenes/triangle_scene.h
+++ src/scenes/triangle_scene.h
@@ -4,9 +4,10 @@
 
 namespace Charcoal {
 class TriangleScene : public Scene {
-    std::vector<Vertex> verts{{{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},
-            {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
-            {{0.0f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}}};
+    std::vector<Vertex> verts{
+            {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+            {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f}},
+            {{0.0f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.5f, 0.0f}}};
     std::vector<int> indices{2, 0, 1};
     std::vector<Mesh> meshes = {{verts, indices}};
 
