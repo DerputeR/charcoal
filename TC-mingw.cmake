f58b3a28d022c765bbae2b58142f85a746b1d8f8
DerputeR <dekxy-developer@yahoo.com>
2026-01-08 00:14:21 -0500

    wip: mingw crosscompile

--- TC-mingw.cmake
+++ TC-mingw.cmake
@@ -1,6 +1,7 @@
 # the name of the target operating system
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR AMD64)
+# set(CMAKE_SYSTEM_PROCESSOR AMD64)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 # which compilers to use for C and C++
 set(CMAKE_C_COMPILER   x86_64-w64-mingw32-gcc)
@@ -10,11 +11,14 @@ set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER  x86_64-w64-mingw32-windres)
 
 # static C++ and GCC runtimes
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT} -static-libstdc++ -static-libgcc")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "${CMAKE_EXE_LINKER_FLAGS_INIT} -static-libstdc++ -static-libgcc")
+set(CMAKE_CXX_FLAGS_INIT
+    "${CMAKE_CXX_FLAGS_INIT} -static-libstdc++ -static-libgcc")
+set(CMAKE_EXE_LINKER_FLAGS_INIT
+    "${CMAKE_EXE_LINKER_FLAGS_INIT} -static-libstdc++ -static-libgcc")
 
 # where is the target environment located
-set(CMAKE_FIND_ROOT_PATH /usr/bin/x86_64-w64-mingw32-g++ ${CMAKE_FIND_ROOT_PATH})
+set(CMAKE_FIND_ROOT_PATH /usr/bin/x86_64-w64-mingw32-g++
+    ${CMAKE_FIND_ROOT_PATH})
 
 # adjust the default behavior of the FIND_XXX() commands:
 ## search programs in the host environment
@@ -27,4 +31,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 ## search packages in the target environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
+# chainload vcpkg
+set(MINGW TRUE)
 include("$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
